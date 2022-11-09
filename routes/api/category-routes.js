bb334acfde158947be89ba81398939a8c17e5463
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-11-08 18:24:42 -0600

    fixed bugged code that did not allow categories to delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,10 +38,9 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create({
-      id: req.body.Category_id,
-      Category_name: req.body.Category_name,
-    });
+    const categoryData = await Category.create(req.body);
+    console.log(req.body);
+    console.log(categoryData);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -70,7 +69,7 @@ router.delete("/:id", async (req, res) => {
         id: req.params.id,
       },
     });
-
+    console.log(categoryData);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -50,7 +50,7 @@ router.post("/", (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -119,8 +119,8 @@ router.delete("/:id", async (req, res) => {
         id: req.params.id,
       },
     });
-
-    res.status(200).json(productData);
+    console.log(productData);
+    res.json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
